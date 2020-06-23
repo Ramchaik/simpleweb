2b55047f590998cefd06a3d102366510a9a2f518
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-06-23 23:57:36 +0530

    Add Notes

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ app.get("/", (req, res) => {
 app.listen(8080, () => {
   console.log("Listening on 8080...");
 });
+
+/* 
+  Notes:
+   -- Start up, command to build container from docker image:
+    --- docker run -p 5000:8080 ramchaik/simpleweb
+   -- Command to build image, just outside the current dir 
+    --- docker build -t ramchaik/simpleweb .
+*/
